d29a2e1e82c4729abc1b7b45b67de4506767ac73
Stenio das Neves Silva <stenionevessilva@gmail.com>
2019-03-31 16:02:55 -0300

    Trabalhando com coleções de outros tipo e a minha implementação do desafio fibonacci até 20ª posição usando somente o For

---
+++ colecoes2/DesafioFibonacci.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+
+/*
+  Desafio proposto pela Microsot de continuar a seguencia até 20ª posição.Essa classe foi cirada para executar 
+  desafio.
+ */
+namespace colecoes2{
+
+   class DesafioFibonacci
+   {
+       
+       public void Fibonacci(){
+           var fibonacciNumeros = new List<int> {1, 1};
+            var proximo=0;
+            var proximo2 =0;
+             
+
+
+                for(int i=0;i<20;++i){//atualiza, aqui é a sacada incremento primerio.
+                    proximo=fibonacciNumeros[fibonacciNumeros.Count-1];
+                    proximo2 = fibonacciNumeros[fibonacciNumeros.Count-2];
+                     fibonacciNumeros.Add(proximo+proximo2);
+                     Console.WriteLine(fibonacciNumeros[i]);
+
+                }
+
+
+
+
+       }
+
+
+
+
+
+   }
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ colecoes2/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace colecoes2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var fibonacciNumeros = new List<int> {1, 1};
+            var proximo= fibonacciNumeros[fibonacciNumeros.Count-1];
+            var proximo2 = fibonacciNumeros[fibonacciNumeros.Count-2];
+             fibonacciNumeros.Add(proximo+proximo2);
+            
+
+             foreach(var item in fibonacciNumeros){
+                 //Console.WriteLine(item);
+               
+             }
+
+             DesafioFibonacci ds = new DesafioFibonacci();
+
+             ds.Fibonacci();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
