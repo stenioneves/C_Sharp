215426d0fd874ca88b2d3bdf82a5ded6c036e420
Stenio das Neves Silva <stenionevessilva@gmail.com>
2019-03-31 15:56:51 -0300

    Usando o IF, ELSE, While, Do While e For

---
+++ controle/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace controle
+{
+    class Program
+    {
+        static void Main(string[] args){
+        int a = 5;
+        int b = 3;
+        if (a + b > 10)
+     {
+       Console.WriteLine("The answer is greater than 10");
+      }
+      else
+      {
+       Console.WriteLine("The answer is not greater than 10");
+       }
+
+       int control = 0;
+       do {
+           Console.WriteLine($" Do While:{control}");
+           control++;
+       } while (control<50);
+
+       while(control <60){
+            Console.WriteLine($" While {control}");
+           control++;
+       }
+        for(control=0; control<80;control++){
+            Console.WriteLine($" for {control}");
+        }
+
+
+
+
+
+    }
+}
+}
